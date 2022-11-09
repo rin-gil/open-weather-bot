e39921e0b9182b5dba4fde1ab78fa5e42aa09c36
rin-gil <ringil.endimion@gmail.com>
2022-11-09 23:27:38 +0200

    Some improvements

--- tgbot/misc/loging.py
+++ tgbot/misc/loging.py
@@ -2,6 +2,10 @@
 
 import logging
 
+from os import path
+
+from tgbot.config import BASE_DIR
+
 logger: logging.Logger = logging.getLogger(__name__)
 logging.basicConfig(
     # filename=path.join(BASE_DIR, 'OpenWeatherBot.log'),
