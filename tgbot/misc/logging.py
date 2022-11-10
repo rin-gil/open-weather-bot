6448811f6188fd105c09ea9a919bcceca9990c43
rin-gil <ringil.endimion@gmail.com>
2022-11-10 17:03:24 +0200

    Bugfix

--- tgbot/misc/logging.py
+++ tgbot/misc/logging.py
@@ -8,7 +8,7 @@ from tgbot.config import BASE_DIR
 
 logger: logging.Logger = logging.getLogger(__name__)
 logging.basicConfig(
-    # filename=path.join(BASE_DIR, 'OpenWeatherBot.log'),
+    filename=path.join(BASE_DIR, 'OpenWeatherBot.log'),
     level=logging.INFO,
     format='%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s'
 )
