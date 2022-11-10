bffa382a86d28a3fc7f8b32a8513d4929f7813a9
rin-gil <ringil.endimion@gmail.com>
2022-11-10 22:05:44 +0200

    re-commit

--- tgbot/misc/states.py
+++
@@ -1,6 +0,0 @@
-from aiogram.dispatcher.filters.state import StatesGroup, State
-
-
-class UserInput(StatesGroup):
-    """Describes the state for the FSM (Final State Machine)"""
-    Allow = State()
