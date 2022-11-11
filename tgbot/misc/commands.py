679955204b45f021f67a83302f90a2afeb44e1ef
rin-gil <ringil.endimion@gmail.com>
2022-11-11 22:04:36 +0200

    Cosmetic code changes

--- tgbot/misc/commands.py
+++ tgbot/misc/commands.py
@@ -10,8 +10,10 @@ async def set_default_commands(dp: Dispatcher) -> None:
     """ Sets commands for the bot """
     for lang in await locale.get_list_languages():
         await dp.bot.set_my_commands(
-            commands=[BotCommand('start', await locale.get_translate(lang=lang, translate='command_start')),
-                      BotCommand('about', await locale.get_translate(lang=lang, translate='command_about')),
-                      BotCommand('stop', await locale.get_translate(lang=lang, translate='command_stop'))],
+            commands=[
+                BotCommand('start', await locale.get_translate(lang=lang, translate='command_start')),
+                BotCommand('about', await locale.get_translate(lang=lang, translate='command_about')),
+                BotCommand('stop', await locale.get_translate(lang=lang, translate='command_stop'))
+            ],
             language_code=lang
         )
